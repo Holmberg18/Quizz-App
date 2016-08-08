ba443aba8e67685e31e72431e2de482ce9add08f
Jon Holmberg <jon.holmberg@ecinternet.com>
2016-08-08 14:15:54 -0700

    first draft done

--- js/app.js
+++ js/app.js
@@ -1,25 +1,10 @@
 
 $(document).ready(function(){
 
-$("#character_2").hide();
-$("#character_3").hide();
-$("#character_4").hide();
-$("#character_5").hide();
-$("#character_6").hide();
-$("#character_7").hide();
-$("#character_8").hide();
-$("#character_9").hide();
-$("#character_10").hide();
-$("#q2").hide();
-$("#q3").hide();
-$("#q4").hide();
-$("#q5").hide();
-$("#q6").hide();
-$("#q7").hide();
-$("#q8").hide();
-$("#q9").hide();
-$("#q10").hide();
-	
+$(".character-picture").hide();
+$(".game").hide();
+$(".question-status").hide();
+$(".results").hide();	
 
 
 });
